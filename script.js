38bff3e4634c5dbbb4b7a2eb23f58444660a95ef
Ashwini277 <devarushiashwini@gmail.com>
2024-07-13 12:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+it('should verify text in paragraphs and list items', () => {
+    cy.get('p').eq(0).should('have.text', para_text); // Verify paragraph text
+    
+    // Verify each list item text
+    list_texts.forEach((text, idx) => {
+        cy.get('li').eq(idx).should('have.text', text);
+    });
+});
+Make sure that para_text and list_texts are defined correctly wit
